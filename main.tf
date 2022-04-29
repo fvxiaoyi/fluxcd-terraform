d25bc4cbfc849a1f48973ed03c5d025c41cd569b
ebin <ebinSu@xm.remarkablefoods.com>
2022-04-29 13:36:58 +0800

    -

--- main.tf
+++ main.tf
@@ -76,3 +76,53 @@ resource "kubernetes_secret" "main" {
     password = var.github_token
   }
 }
+
+# SSH
+locals {
+  known_hosts = "github.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg="
+}
+
+resource "tls_private_key" "main" {
+  algorithm   = "ECDSA"
+  ecdsa_curve = "P256"
+}
+
+# GitHub
+resource "github_repository" "main" {
+  name       = var.repository_name
+  visibility = var.repository_visibility
+  auto_init  = true
+}
+
+resource "github_branch_default" "main" {
+  repository = github_repository.main.name
+  branch     = var.branch
+}
+
+resource "github_repository_deploy_key" "main" {
+  title      = "staging-cluster"
+  repository = github_repository.main.name
+  key        = tls_private_key.main.public_key_openssh
+  read_only  = true
+}
+
+resource "github_repository_file" "install" {
+  repository = github_repository.main.name
+  file       = data.flux_install.main.path
+  content    = data.flux_install.main.content
+  branch     = var.branch
+}
+
+resource "github_repository_file" "sync" {
+  repository = github_repository.main.name
+  file       = data.flux_sync.main.path
+  content    = data.flux_sync.main.content
+  branch     = var.branch
+}
+
+resource "github_repository_file" "kustomize" {
+  repository = github_repository.main.name
+  file       = data.flux_sync.main.kustomize_path
+  content    = data.flux_sync.main.kustomize_content
+  branch     = var.branch
+}
--- providers.tf
+++ providers.tf
@@ -14,6 +14,16 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = ">= 2.0.2"
     }
+
+    github = {
+      source  = "integrations/github"
+      version = ">= 4.5.2"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "3.1.0"
+    }
   }
 }
 
@@ -26,4 +36,9 @@ provider "kubectl" {
 
 provider "kubernetes" {
   config_path = "/etc/rancher/k3s/k3s.yaml"
+}
+
+provider "github" {
+  owner = var.github_owner
+  token = var.github_token
 }
