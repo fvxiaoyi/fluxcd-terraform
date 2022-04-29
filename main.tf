ca99600dad7a36d65dd0c3da3aada36219a957db
ebin <ebinSu@xm.remarkablefoods.com>
2022-04-29 14:44:12 +0800

    -

--- main.tf
+++ main.tf
@@ -1,3 +1,44 @@
+terraform {
+  required_version = ">= 0.13"
+
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = ">= 4.5.2"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.2"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.10.0"
+    }
+    flux = {
+      source  = "fluxcd/flux"
+      version = ">= 0.0.13"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "3.1.0"
+    }
+
+  }
+}
+
+provider "flux" {}
+
+provider "kubectl" {}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+provider "github" {
+  owner = var.github_owner
+  token = var.github_token
+}
+
 # SSH
 locals {
   known_hosts = "github.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg="
@@ -74,8 +115,9 @@ resource "kubernetes_secret" "main" {
   }
 
   data = {
-    username = var.github_owner
-    password = var.github_token
+    identity       = tls_private_key.main.private_key_pem
+    "identity.pub" = tls_private_key.main.public_key_pem
+    known_hosts    = local.known_hosts
   }
 }
 
--- output.tf
+++
@@ -1,7 +0,0 @@
-output "apply" {
-  value = data.flux_sync.main.secret
-}
-
-output "sync" {
-  value = data.flux_sync.main.namespace
-}
