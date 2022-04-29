584aea9ccdd672031a7fe7dbf03fb03f1065d657
ebin <ebinSu@xm.remarkablefoods.com>
2022-04-29 14:18:51 +0800

    -

--- main.tf
+++ main.tf
@@ -1,15 +1,25 @@
-# Generate manifests
+# SSH
+locals {
+  known_hosts = "github.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg="
+}
+
+resource "tls_private_key" "main" {
+  algorithm   = "ECDSA"
+  ecdsa_curve = "P256"
+}
+
+# Flux
 data "flux_install" "main" {
-  target_path      = var.target_path
-  network_policy   = false
-  components_extra = ["image-reflector-controller", "image-automation-controller"]
+  target_path = var.target_path
 }
 
 data "flux_sync" "main" {
   target_path = var.target_path
-  url         = "https://github.com/${var.github_owner}/${var.repository_name}"
+  url         = "ssh://git@github.com/${var.github_owner}/${var.repository_name}.git"
+  branch      = var.branch
 }
 
+# Kubernetes
 resource "kubernetes_namespace" "flux_system" {
   metadata {
     name = "flux-system"
@@ -22,9 +32,7 @@ resource "kubernetes_namespace" "flux_system" {
   }
 }
 
-# Split multi-doc YAML with
-# https://registry.terraform.io/providers/gavinbunney/kubectl/latest
-data "kubectl_file_documents" "apply" {
+data "kubectl_file_documents" "install" {
   content = data.flux_install.main.content
 }
 
@@ -32,26 +40,21 @@ data "kubectl_file_documents" "sync" {
   content = data.flux_sync.main.content
 }
 
-# Convert documents list to include parsed yaml data
 locals {
-  apply = [for v in data.kubectl_file_documents.apply.documents : {
+  install = [for v in data.kubectl_file_documents.install.documents : {
     data : yamldecode(v)
     content : v
-    }
+  }
   ]
-}
-
-locals {
   sync = [for v in data.kubectl_file_documents.sync.documents : {
     data : yamldecode(v)
     content : v
-    }
+  }
   ]
 }
 
-# Apply manifests on the cluster
-resource "kubectl_manifest" "apply" {
-  for_each   = { for v in local.apply : lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name]))) => v.content }
+resource "kubectl_manifest" "install" {
+  for_each   = { for v in local.install : lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name]))) => v.content }
   depends_on = [kubernetes_namespace.flux_system]
   yaml_body  = each.value
 }
@@ -62,9 +65,8 @@ resource "kubectl_manifest" "sync" {
   yaml_body  = each.value
 }
 
-# Generate a Kubernetes secret with the Git credentials
 resource "kubernetes_secret" "main" {
-  depends_on = [kubectl_manifest.apply]
+  depends_on = [kubectl_manifest.install]
 
   metadata {
     name      = data.flux_sync.main.secret
@@ -72,19 +74,10 @@ resource "kubernetes_secret" "main" {
   }
 
   data = {
-    username = "git"
-    password = var.github_token
+    identity       = tls_private_key.main.private_key_pem
+    "identity.pub" = tls_private_key.main.public_key_pem
+    known_hosts    = local.known_hosts
   }
-}
-
-# SSH
-locals {
-  known_hosts = "github.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg="
-}
-
-resource "tls_private_key" "main" {
-  algorithm   = "ECDSA"
-  ecdsa_curve = "P256"
 }
 
 # GitHub
